e8de70b04aa2e1500448f023f8b754ce1bc950a9
Saikiran <saikiran.chavan@contactout.io>
2023-09-17 11:55:32 +0530

    Added laravel application files

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\Interfaces\FinanceServiceInterface;
+use App\Services\YahooFinanceService;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
